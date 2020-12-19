fc1daf633b6cfc0dedeb63ded78b2e55191d4059
JonniOyanedel <jeoyaned@gmail.com>
2020-12-18 21:05:38 -0300

    Prueba
    Prueba

---
+++ BiceIndicadores.Api/BusinesObjects/Entities/Indicador.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bice.BusinessObjects
+{
+    public class Cobre
+    {
+        public string key { get; set; }
+        public string name { get; set; }
+        public string unit { get; set; }
+        public string date { get; set; }
+        public double value { get; set; }
+
+    }
+
+    public class Dolar
+    {
+        public string key { get; set; }
+        public string name { get; set; }
+        public string unit { get; set; }
+        public string date { get; set; }
+        public double value { get; set; }
+
+    }
+
+    public class Euro
+    {
+        public string key { get; set; }
+        public string name { get; set; }
+        public string unit { get; set; }
+        public string date { get; set; }
+        public double value { get; set; }
+
+    }
+
+    public class Ipc
+    {
+        public string key { get; set; }
+        public string name { get; set; }
+        public string unit { get; set; }
+        public string date { get; set; }
+        public double value { get; set; }
+
+    }
+
+    public class Ivp
+    {
+        public string key { get; set; }
+        public string name { get; set; }
+        public string unit { get; set; }
+        public string date { get; set; }
+        public double value { get; set; }
+
+    }
+
+    public class Oro
+    {
+        public string key { get; set; }
+        public string name { get; set; }
+        public string unit { get; set; }
+        public string date { get; set; }
+        public double value { get; set; }
+
+    }
+
+    public class Plata
+    {
+        public string key { get; set; }
+        public string name { get; set; }
+        public string unit { get; set; }
+        public string date { get; set; }
+        public double value { get; set; }
+
+    }
+
+    public class Uf
+    {
+        public string key { get; set; }
+        public string name { get; set; }
+        public string unit { get; set; }
+        public string date { get; set; }
+        public double value { get; set; }
+
+    }
+
+    public class Utm
+    {
+        public string key { get; set; }
+        public string name { get; set; }
+        public string unit { get; set; }
+        public string date { get; set; }
+        public double value { get; set; }
+
+    }
+
+    public class Yen
+    {
+        public string key { get; set; }
+        public string name { get; set; }
+        public string unit { get; set; }
+        public string date { get; set; }
+        public double value { get; set; }
+
+    }
+
+    public class Indicador_Entity
+    {
+        //public Cobre cobre { get; set; }
+        //public Dolar dolar { get; set; }
+        //public Euro euro { get; set; }
+        //public Ipc ipc { get; set; }
+        //public Ivp ivp { get; set; }
+        //public Oro oro { get; set; }
+        //public Plata plata { get; set; }
+        //public Uf uf { get; set; }
+        //public Utm utm { get; set; }
+        //public Yen yen { get; set; }
+
+        public List<IndicadorContainer> Lista { get; set; }
+
+    }
+
+    public class IndicadorContainer
+    {
+        public string Nombre { get; set; }
+        public Indicador Valores { get; set; }
+    }
+
+    public class Indicador
+    {
+        public string key { get; set; }
+        public string name { get; set; }
+        public string unit { get; set; }
+        public string date { get; set; }
+        public double value { get; set; }
+
+    }
+}
---
+++ BiceIndicadores.Api/Context/ExternalApiContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Bice.BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Bice.Context
+{
+    public class ExternalApiContext
+    {
+        private static IConfiguration _configuration;
+        private static IConfigurationSection _configSection;
+        Indicador_Entity Indicador = new Indicador_Entity();
+
+        public ExternalApiContext()
+        {
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            _configSection = _configuration.GetSection("appSettings");
+
+            var urlApiIndecon = _configSection["externalURL"];
+           
+            var client = new HttpClient();
+            HttpResponseMessage respIndicadores = client.GetAsync(new Uri(urlApiIndecon)).Result;
+            var resul = respIndicadores.Content.ReadAsStringAsync().Result;
+
+            var prueba = JsonConvert.DeserializeObject<Indicador_Entity>(resul);
+            //Indicador = JsonConvert.DeserializeObject<Indicador_Entity>(resul);
+
+         }
+
+        public Indicador_Entity Indicadores
+        {
+            get
+            {
+                return Indicador;
+            }
+
+        }
+
+
+    }
+}
